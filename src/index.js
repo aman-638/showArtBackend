a3bd66a5b1b95c28464061c60755d97288e9b0b7
aman-638 <ar7009297@gmail.com>
2022-04-22 15:24:55 +0530

    authication working

---
+++ src/index.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const express = require("express");
+const connect = require("./config/db");
+const {login, register} = require("./controllers/auth.controller");
+const userController = require("./controllers/user.controller");
+
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+//starting api routers and methods from here.
+//sending this call to respective controllers to find route and desired method.
+
+//user controller
+app.use("/users", userController);
+
+//product of art controller
+
+
+//login and register controller
+app.post("/login", login)
+app.post("/register", register)
+
+
+
+
+//connecting ans starting server
+const port = process.env.PORT || 1697;
+
+app.listen(port, () => {
+    try{
+        connect();
+        console.log(`Server is running on port ${port}`);
+    }catch(err){
+
+    }
+})
